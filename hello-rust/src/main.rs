d0f67e69964f9faf1c094d83e4e52f30dfeac28b
llianc62 <lianglian0217@qq.com>
2024-07-27 01:45:41 +0800

    add hello world

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,6 +1,15 @@
 use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
+fn greet_world() {
+    let english = "Hello World!";
+    let chinese = "你好，世界！";
+    let regions = [chinese, english];
+    for region in regions.iter() {
+        println!("{}", &region);
+    }
+}
+
 fn main() {
     let stdout = stdout();
     let message = String::from("Hello Fellow Rustaceans!");
@@ -8,4 +17,6 @@ fn main() {
 
     let mut writer = BufWriter::new(stdout.lock());
     say(&message, width, &mut writer).unwrap();
+
+    greet_world();
 }
